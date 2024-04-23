1433f46e5d5777635ee113d5e44726c96d83aa11
cassianodescovi <cassianodescovi@gmail.com>
2024-04-22 20:59:52 -0400

    Create main.py and routers

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from database import engine
+import models
+from router import router
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app.include_router(router)
